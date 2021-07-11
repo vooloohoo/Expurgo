c18a7942b5e3cc90905ee5321eaec367ae20fcb7
vooloohoo <69146981+vooloohoo@users.noreply.github.com>
2021-07-11 14:07:32 +0200

    Cv2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import folium
-from cv2 import cv2
+import cv2
 import matplotlib.pyplot as plt
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
